6f495106a3e5f8e990f7c122f0385520e6c08a4c
Anthony <anthonymr2010@hotmail.com>
2020-02-27 23:26:33 -0600

    prueba 2

---
+++ HolaMundo2/srcc/defaultpackage/HolaMundo.java
@@ -0,0 +1,11 @@
+package defaultpackage;
+
+public class HolaMundo {
+	
+	public static void main(String args[]) {
+		boolean caracter = true;
+		
+		System.out.println("Numero entero: "+caracter);
+	}
+
+}
